13a6334000f3745d761f15c85583ebf270d38a3d
Christophe Desguez <christophe.desguez@gmail.com>
2014-09-23 23:51:37 +0200

    Slideshow plugin now part of the build

--- js/jquery.onscreen.slideshow.js
+++ js/jquery.onscreen.slideshow.js
@@ -1,100 +1,130 @@
-function SlideShow(slides, options) {
+(function($, w, undefined) {
 
-	var settings = $.extend({}, SlideShow.defaults, options),
-		len = slides.length,
-		currentIndex = 0, 
-		autoplay = true;
+	var onScreen = $.onScreen;
 
-	/**
-	 * Go to the specified slide, or to the next one
-	 */
-	function displaySlide(index, options) {
+	if (!onScreen) return;
 
-		if (typeof index === "string" && index === "last") {
-			currentIndex = len - 1;
+	var _DEFAULTS = {
+		slideDuration: 4000,
+		transitionSpeed: 1500,
+		keyControls: true, // support default key pressed actions
+		videoPlayer: {
+			bgcolor: "black",
+			width: 960,
+			height: 540,
+			autoplay: 1,
+			color: "111"
+		}
+	};
 
-		} else if (typeof index === "string" && index === "fist") {
-			currentIndex = 0;
+	function SlideShow(slides, options) {
 
-		} else if (index === undefined || index < 0 || index >= len) {
-			// invalid value > auto play
-			currentIndex = (currentIndex + 1) % len;
+		var settings = $.extend({}, SlideShow.defaults, options),
+			len = slides.length,
+			currentIndex = 0, 
+			autoplay = true;
+
+		/**
+		 * Go to the specified slide, or to the next one
+		 */
+		function displaySlide(index, options) {
+
+			if (typeof index === "string" && index === "last") {
+				currentIndex = len - 1;
+
+			} else if (typeof index === "string" && index === "fist") {
+				currentIndex = 0;
+
+			} else if (index === undefined || index < 0 || index >= len) {
+				// invalid value > auto play
+				currentIndex = (currentIndex + 1) % len;
+
+			} else {
+				currentIndex = index;
+			}
+
+			return onScreen(slides[currentIndex], options);
+		}
+
+
+		/**
+		 * Program next slide
+		 */
+		function nextTransition(slide) {
+			displaySlide.future = (autoplay) ? setTimeout(function () {
+				requestAnimationFrame(displaySlide);
+			}, settings.slideDuration) : 0;
+		}
+
+		function stop() {
+			autoplay = false;
+			clearTimeout(displaySlide.future);
+		}
+		function play() {
+			autoplay = true;
+			displaySlide();
+		}
+		function next(evt) {
+			stop();
+			displaySlide(currentIndex + 1);
+			if (evt) evt.preventDefault();
+		}
+		function previous(evt) {
+			stop();
+			displaySlide(currentIndex ? currentIndex - 1 : len - 1);
+			if (evt) evt.preventDefault();
+		}
+
+		function playPause() {
+			(autoplay) ? stop() : play();
+		}
+
+		// Keyboard Handler
+		function keyHandler(evt) {
+			switch (evt.keyCode) {
+				case 37: // LEFT
+					previous();
+					break;
+				case 39: // RIGHT
+					next();
+					break;
+				case 32: // SPACE
+					playPause();
+			}		
+		}
+		
+		if (settings.keyControls) $(document).on("keydown", keyHandler);
+
+		onScreen("settings", settings); // this first call just sets the defaults settings
+
+		displaySlide(0);
+
+		return { // return the interface
+			stop: stop,
+			play: play,
+			playPause: playPause,
+			displaySlide: displaySlide,
+			previous: previous,
+			next: next
+		};
+	}
+
+	SlideShow.defaults = _DEFAULTS;
+
+	// Redefine $.onScreen to handle an array of slides
+	$.onScreen = function(slides, options) {
+		if ($.isArray(slides)) {
+			new SlideShow(slides, options);
 
 		} else {
-			currentIndex = index;
+			onScreen(slides, options);
 		}
-
-		return $.onScreen(slides[currentIndex], options);
 	}
-
-
-	/**
-	 * Program next slide
-	 */
-	function nextTransition(slide) {
-		displaySlide.future = (autoplay) ? setTimeout(function () {
-			requestAnimationFrame(displaySlide);
-		}, settings.duration) : 0;
-	}
-
-	function stop() {
-		autoplay = false;
-		clearTimeout(displaySlide.future);
-	}
-	function play() {
-		autoplay = true;
-		displaySlide();
-	}
-	function next(evt) {
-		stop();
-		displaySlide(currentIndex + 1);
-		if (evt) evt.preventDefault();
-	}
-	function previous(evt) {
-		stop();
-		displaySlide(currentIndex ? currentIndex - 1 : len - 1);
-		if (evt) evt.preventDefault();
-	}
-
-	function playPause() {
-		(autoplay) ? stop() : play();
-	}
-
-	// Keyboard Handler
-	$(document).on("keydown", function (evt) {
-		switch (evt.keyCode) {
-			case 37: // LEFT
-				previous();
-				break;
-			case 39: // RIGHT
-				next();
-				break;
-			case 32: // SPACE
-				playPause();
-		}
+	$.extend($.onScreen, {
+		_plugins: onScreen._plugins,
+		getPlugin: onScreen.getPlugin,
+		addPlugin: onScreen.addPlugin
 	});
 
-	$.onScreen("settings", settings); // this first call just sets the defaults settings
 
-
-	return { // return the interface
-		stop: stop,
-		play: play,
-		playPause: playPause,
-		displaySlide: displaySlide,
-		previous: previous,
-		next: next
-	}
-}
-
-SlideShow.defaults = {
-	duration: 4000,
-	speed: 1500,
-	videoPlayer: {
-		bgcolor: "black",
-		width: 960,
-		height: 540,
-		autoplay: 1,
-		color: "111"
-	}
-};
+})(window.jQuery || window.Zepto, this);
