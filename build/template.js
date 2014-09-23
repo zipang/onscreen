a4522e69fa9e4af6a0b6a67f5f8f0b67deac58b6
Christophe Desguez <christophe.desguez@gmail.com>
2014-09-23 23:43:26 +0200

    Bumped to v1.0rc1

--- build/template.js
+++ build/template.js
@@ -1,8 +1,14 @@
 /**
- * JQUERY on$creen - {{version}} - {{date}}
+ * $.onScreen - v{{version}} - {{date}}
 
  * @author zipang (EIDOLON LABS)
  * @url http://github.com/zipang/onscreen
+ * @copyright ({{year}}) EIDOLON LABS
+
+ * Project images or video full screen or on any HTML element
+ * Supports additional HTML content
+ * Automatic preloading of resources (images) 
+ * Smooth transitions using CSS3 or JS 
  */
  {{body}}
  
