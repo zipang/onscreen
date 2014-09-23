8a15f2a71ad0694881fcad67d1ce971c4b89fcf5
Christophe Desguez <christophe.desguez@gmail.com>
2014-09-23 23:50:49 +0200

    Added symbolic link to latest version

--- build/build.js
+++ build/build.js
@@ -1,15 +1,23 @@
 #!/usr/bin/env node
 
 var buildify = require('buildify'),
-    version = "0.95";
+	fs = require('fs'),
+    version = "1.0rc1",
+    buildDate = new Date;
 
 buildify("../js/")
   .load('jquery.onscreen.js')
-  .concat(['jquery.onscreen.youtube.js', 'jquery.onscreen.vimeo.js'])
-  .wrap('../build/template.js', { version: version, date: new Date })
+  .concat(['jquery.onscreen.slideshow.js', 'jquery.onscreen.youtube.js', 'jquery.onscreen.vimeo.js'])
+  .wrap('../build/template.js', { version: version, date: buildDate, year: buildDate.getFullYear() })
   .changeDir('../dist/')
   .save('jquery.onscreen-' + version + '.js')
+  .symLink('jquery.onscreen-' + version + '.js', 'jquery.onscreen-latest.js')
   .uglify()
-  .save('jquery.onscreen-' + version + '.min.js');
+  .save('jquery.onscreen-' + version + '.min.js')
+  .symLink('jquery.onscreen-' + version + '.min.js', 'jquery.onscreen-latest.min.js');
+
+// process.chdir('../dist/');
+// fs.symlinkSync('jquery.onscreen-' + version + '.js', 'jquery.onscreen-latest.js');
+// fs.symlinkSync('jquery.onscreen-' + version + '.min.js', 'jquery.onscreen-latest.min.js');
 
 console.log('Build successfull');
